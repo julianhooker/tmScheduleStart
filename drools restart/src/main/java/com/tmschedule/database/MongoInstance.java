ec1c417a0cbac82b8dbf1214d4300cfc76b432d1
Julian Hooker <julian_hooker@yahoo.com>
2015-08-30 09:50:28 -0500

    Cleaned up the class internals, leaving only the necessary methods/constructors.

--- drools restart/src/main/java/com/tmschedule/database/MongoInstance.java
+++ drools restart/src/main/java/com/tmschedule/database/MongoInstance.java
@@ -3,18 +3,10 @@ package com.tmschedule.database;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 
+// Singleton class for the Mongo database connection
 public class MongoInstance {
-	
 	private static MongoClient mongoClient = new MongoClient("localhost", 27017);
 	private static MongoDatabase db = mongoClient.getDatabase("julian");
-
-	private static MongoInstance instance = new MongoInstance(); 
-	
-	private MongoInstance() {}
-	
-	public static MongoInstance getInstance() {
-		return instance;
-	}
 	
 	public static MongoDatabase getDB() {
 		return db; 
